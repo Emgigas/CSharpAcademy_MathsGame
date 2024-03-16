53c11b720e83204195d9a6ffb26c0295ea826170
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-16 17:00:42 +0000

    Added switch statement to capture user opening response

--- CSharpAcademy_MathsGame/Program.cs
+++ CSharpAcademy_MathsGame/Program.cs
@@ -5,7 +5,7 @@ DateTime date = DateTime.UtcNow;
 string linebreak = "============================================================================================";
 
 Console.WriteLine(linebreak);
-Console.WriteLine($"Hello {name}. It's {date}. This is the CSharpAcademy Maths game challenge.\n");
+Console.WriteLine($"Hello {name}. It's {date}. This is the CSharpAcademy Maths game challenge.");
 Console.WriteLine($@"Which game would you like to play. Select from the options below:
 A - Addition
 S - Subtraction
@@ -13,3 +13,18 @@ M - Multiplication
 D - Division
 Q - Quit the program");
 Console.WriteLine(linebreak);
+
+string? gameSelected = Console.ReadLine();
+
+string? response = gameSelected.Trim().ToLower() switch
+{
+    "a" => "Addition game selected",
+    "s" => "Subtraction game selected",
+    "m" => "Multiplication game selected",
+    "d" => "Division game selected",
+    "q" => "Game has ended",
+    _ => "Incorrect response given",
+};
+
+Console.WriteLine(response);
+
