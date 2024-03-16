2763eb397dbb562b92d85544e38d321088381625
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-16 15:06:50 +0000

    Created opening message and request for the players name

--- CSharpAcademy_MathsGame/Program.cs
+++ CSharpAcademy_MathsGame/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please enter your name: ");
+
+string? name = Console.ReadLine();
+DateTime date = DateTime.UtcNow;
+
+Console.WriteLine("=================================================");
+Console.WriteLine($"Hello {name}. It's {date}. This is the CSharpAcademy Maths game challenge.");
