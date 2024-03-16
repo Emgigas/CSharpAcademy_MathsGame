be80c5ccc1a8b1aa51c5a0f9567b4d36723a6138
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-16 18:33:53 +0000

    Created method to find division combinations that lead to integer result

--- CSharpAcademy_MathsGame/Program.cs
+++ CSharpAcademy_MathsGame/Program.cs
@@ -45,21 +45,17 @@ void AdditionGame()
 
     Random random = new Random();
 
-    int firstNumber;
-    int secondNumber;
     int score = 0;
 
     for (int i = 0; i < 5; i++)
     {
-        firstNumber = random.Next(1, 9);
-        secondNumber = random.Next(1, 9);
+        var firstNumber = random.Next(1, 9);
+        var secondNumber = random.Next(1, 9);
         
         Console.Write($"\n{firstNumber} + {secondNumber} = ");
         var result = Console.ReadLine();
 
-        int parseResult;
-
-        bool checkResult = int.TryParse(result, out parseResult);
+        bool checkResult = int.TryParse(result, out var parseResult);
         if (checkResult)
         {
             if (parseResult == firstNumber + secondNumber)
@@ -79,21 +75,17 @@ void SubtractionGame()
     Console.WriteLine("Subtraction game selected");
     Random random = new Random();
 
-    int firstNumber;
-    int secondNumber;
     int score = 0;
 
     for (int i = 0; i < 5; i++)
     {
-        firstNumber = random.Next(1, 9);
-        secondNumber = random.Next(1, 9);
+        var firstNumber = random.Next(1, 9);
+        var secondNumber = random.Next(1, 9);
         
         Console.Write($"\n{firstNumber} - {secondNumber} = ");
         var result = Console.ReadLine();
 
-        int parseResult;
-
-        bool checkResult = int.TryParse(result, out parseResult);
+        bool checkResult = int.TryParse(result, out var parseResult);
         if (checkResult)
         {
             if (parseResult == firstNumber - secondNumber)
@@ -101,7 +93,7 @@ void SubtractionGame()
                 Console.WriteLine("Your answer was correct!");
                 score++;
             }
-            else Console.WriteLine($"Incorrect answer. The correct answer was {firstNumber + secondNumber}.");
+            else Console.WriteLine($"Incorrect answer. The correct answer was {firstNumber - secondNumber}.");
         }
         else Console.WriteLine("Your answer was not a valid number.");
     }
@@ -113,21 +105,17 @@ void MultiplicationGame()
     Console.WriteLine("Multiplication game selected");
     Random random = new Random();
 
-    int firstNumber;
-    int secondNumber;
     int score = 0;
 
     for (int i = 0; i < 5; i++)
     {
-        firstNumber = random.Next(1, 9);
-        secondNumber = random.Next(1, 9);
+        var firstNumber = random.Next(1, 9);
+        var secondNumber = random.Next(1, 9);
         
         Console.Write($"\n{firstNumber} * {secondNumber} = ");
         var result = Console.ReadLine();
 
-        int parseResult;
-
-        bool checkResult = int.TryParse(result, out parseResult);
+        bool checkResult = int.TryParse(result, out var parseResult);
         if (checkResult)
         {
             if (parseResult == firstNumber * secondNumber)
@@ -135,7 +123,7 @@ void MultiplicationGame()
                 Console.WriteLine("Your answer was correct!");
                 score++;
             }
-            else Console.WriteLine($"Incorrect answer. The correct answer was {firstNumber + secondNumber}.");
+            else Console.WriteLine($"Incorrect answer. The correct answer was {firstNumber * secondNumber}.");
         }
         else Console.WriteLine("Your answer was not a valid number.");
     }
@@ -149,21 +137,18 @@ void DivisionGame()
     
     Random random = new Random();
 
-    int firstNumber;
-    int secondNumber;
     int score = 0;
 
     for (int i = 0; i < 5; i++)
     {
-        firstNumber = random.Next(1, 9);
-        secondNumber = random.Next(1, 9);
+        int[] divisionNumbers = GetDivisionNumbers();
+        int firstNumber = divisionNumbers[0];
+        int secondNumber = divisionNumbers[1];
         
         Console.Write($"\n{firstNumber} / {secondNumber} = ");
         var result = Console.ReadLine();
 
-        int parseResult;
-
-        bool checkResult = int.TryParse(result, out parseResult);
+        bool checkResult = int.TryParse(result, out var parseResult);
         if (checkResult)
         {
             if (parseResult == firstNumber / secondNumber)
@@ -171,12 +156,31 @@ void DivisionGame()
                 Console.WriteLine("Your answer was correct!");
                 score++;
             }
-            else Console.WriteLine($"Incorrect answer. The correct answer was {firstNumber + secondNumber}.");
+            else Console.WriteLine($"Incorrect answer. The correct answer was {firstNumber / secondNumber}.");
         }
         else Console.WriteLine("Your answer was not a valid number.");
     }
 
     Console.WriteLine($"\nGame over. Your final score was: {score}");
+}
+
+int[] GetDivisionNumbers()
+{
+    Random random = new Random();
+    int firstNumber = random.Next(0, 99);
+    int secondNumber = random.Next(0, 99);
+    int[] result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = random.Next(0, 99);
+        secondNumber = random.Next(0, 99);
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    return result;
 }
 
 void QuitGame()
