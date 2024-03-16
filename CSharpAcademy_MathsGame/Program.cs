c30833c52d89d1db624f26769c6b408f629f5d70
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-16 16:43:22 +0000

    Added user choice strings to start of file

--- CSharpAcademy_MathsGame/Program.cs
+++ CSharpAcademy_MathsGame/Program.cs
@@ -2,6 +2,14 @@
 
 string? name = Console.ReadLine();
 DateTime date = DateTime.UtcNow;
+string linebreak = "============================================================================================";
 
-Console.WriteLine("=================================================");
-Console.WriteLine($"Hello {name}. It's {date}. This is the CSharpAcademy Maths game challenge.");
+Console.WriteLine(linebreak);
+Console.WriteLine($"Hello {name}. It's {date}. This is the CSharpAcademy Maths game challenge.\n");
+Console.WriteLine($@"Which game would you like to play. Select from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine(linebreak);
