3ad89a79f7a5cbf96e49afc04f0ab1afbd624474
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-16 18:35:13 +0000

    Fixed bug where GetDivisionNumbers was trying to divide by 0

--- CSharpAcademy_MathsGame/Program.cs
+++ CSharpAcademy_MathsGame/Program.cs
@@ -167,14 +167,14 @@ void DivisionGame()
 int[] GetDivisionNumbers()
 {
     Random random = new Random();
-    int firstNumber = random.Next(0, 99);
-    int secondNumber = random.Next(0, 99);
+    int firstNumber = random.Next(1, 99);
+    int secondNumber = random.Next(1, 99);
     int[] result = new int[2];
 
     while (firstNumber % secondNumber != 0)
     {
-        firstNumber = random.Next(0, 99);
-        secondNumber = random.Next(0, 99);
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
     }
 
     result[0] = firstNumber;
