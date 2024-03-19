b1bc4522981e3099b49f8b120b05638aa4c3a88b
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-19 18:52:08 +0000

    Created a to do list to finalise project

--- CSharpAcademy_MathsGame/Helpers.cs
+++ CSharpAcademy_MathsGame/Helpers.cs
@@ -4,7 +4,22 @@ namespace CSharpAcademy_MathsGame;
 
 public class Helpers
 {
-    internal static List<Game> gameScores = new();
+    internal static List<Game> gameScores = new()
+    { // List copied from YouTube to learn about LINQ (Language integrated query)
+        // new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5},
+        // new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4},
+        // new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4},
+        // new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3},
+        // new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1},
+        // new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2},
+        // new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3},
+        // new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4},
+        // new Game { Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4},
+        // new Game { Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1},
+        // new Game { Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0},
+        // new Game { Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2},
+        // new Game { Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5},
+    };
     
     internal static void ShowScores()
     {
--- CSharpAcademy_MathsGame/Program.cs
+++ CSharpAcademy_MathsGame/Program.cs
@@ -13,3 +13,12 @@ string? GetName()
     name = Console.ReadLine();
     return name;
 }
+
+// https://www.youtube.com/watch?v=_T7896oRq3Q&list=PL4G0MUH8YWiD1p5ySamqNWAaWlnwp1Vip&index=21
+// TODO: Validate user inputs
+// TODO: Add in difficulty levels
+// TODO: Add in round timers
+// TODO: Add timer results to ShowScores
+// TODO: Allow a user to select round size
+// TODO: Round size to show on score so if roundsize was 10 then score should be x/10
+// TODO: Build a random game mode which generates a round using all gametypes
